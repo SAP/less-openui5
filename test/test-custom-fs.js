80e7b815cb6f164d79a7bd28606aaa3dbb147485
Matthias Osswald <mat.osswald@sap.com>
2017-12-12 15:36:33 +0100

    Extend custom fs tests
    Add test for a simple interface and check that it is actually called.

--- test/test-custom-fs.js
+++ test/test-custom-fs.js
@@ -17,32 +17,66 @@
 
 var assert = require('assert');
 var readFile = require('./common/helper').readFile;
-var gracefulFs = require('graceful-fs');
+var fs = require('fs');
+
 // tested module
 var Builder = require('../').Builder;
 
-describe('CSS Scoping of', function() {
+describe('(custom fs) CSS Scoping of', function() {
 
-      describe('comments', function() {
+  describe('comments', function() {
 
-        it('should return same CSS for foo', function() {
+    it('should return same CSS for foo', function() {
 
-          return new Builder({
-              fs: gracefulFs
-          }).build({
-            lessInputPath: 'comments/themes/foo/library.source.less',
-            rootPaths: [
-              'test/fixtures/libraries/scopes/comments/lib1',
-              'test/fixtures/libraries/scopes/comments/lib2',
-              'test/fixtures/libraries/lib1'
-            ]
-          }).then(function(result) {
-            assert.equal(result.css, readFile('test/expected/libraries/scopes/comments/lib1/comments/themes/foo/library.css'), 'CSS scoping should be correctly generated');
-            assert.equal(result.cssRtl, readFile('test/expected/libraries/scopes/comments/lib1/comments/themes/foo/library-RTL.css'), 'Rtl CSS scoping should be correctly generated');
-          });
-
-        });
+      return new Builder({
+        fs: require('graceful-fs')
+      }).build({
+        lessInputPath: 'comments/themes/foo/library.source.less',
+        rootPaths: [
+          'test/fixtures/libraries/scopes/comments/lib1',
+          'test/fixtures/libraries/scopes/comments/lib2',
+          'test/fixtures/libraries/lib1'
+        ]
+      }).then(function(result) {
+        assert.equal(result.css, readFile('test/expected/libraries/scopes/comments/lib1/comments/themes/foo/library.css'), 'CSS scoping should be correctly generated');
+        assert.equal(result.cssRtl, readFile('test/expected/libraries/scopes/comments/lib1/comments/themes/foo/library-RTL.css'), 'Rtl CSS scoping should be correctly generated');
+      });
 
     });
+
+    it('should return same CSS for bar', function() {
+
+      var readFileCalls = 0,
+          statCalls = 0;
+
+      return new Builder({
+        fs: {
+          readFile: function() {
+            readFileCalls++;
+            return fs.readFile.apply(fs, arguments);
+          },
+          stat: function() {
+            statCalls++;
+            return fs.stat.apply(fs, arguments);
+          }
+        }
+      }).build({
+        lessInputPath: 'comments/themes/bar/library.source.less',
+        rootPaths: [
+          'test/fixtures/libraries/scopes/comments/lib1',
+          'test/fixtures/libraries/scopes/comments/lib2',
+          'test/fixtures/libraries/lib1',
+          'test/fixtures/libraries/lib2'
+        ]
+      }).then(function(result) {
+        assert.equal(result.css, readFile('test/expected/libraries/scopes/comments/lib2/comments/themes/bar/library.css'), 'CSS scoping should be correctly generated');
+        assert.equal(result.cssRtl, readFile('test/expected/libraries/scopes/comments/lib2/comments/themes/bar/library-RTL.css'), 'Rtl CSS scoping should be correctly generated');
+        assert.equal(readFileCalls, 3, "fs.readFile should have been called 3 times");
+        assert.equal(statCalls, 19, "fs.stat should have been called 19 times");
+      });
+
+    });
+
+  });
 
 });
