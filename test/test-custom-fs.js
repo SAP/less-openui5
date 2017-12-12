ae7dc9998f90f14b0ee9df70057d65addc2ac1fc
Matthias Osswald <mat.osswald@sap.com>
2017-12-12 15:26:07 +0100

    Add test for custom fs option

---
+++ test/test-custom-fs.js
@@ -0,0 +1,48 @@
+// Copyright 2017 SAP SE.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http: //www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+/*eslint-env mocha */
+'use strict';
+
+var assert = require('assert');
+var readFile = require('./common/helper').readFile;
+var gracefulFs = require('graceful-fs');
+// tested module
+var Builder = require('../').Builder;
+
+describe('CSS Scoping of', function() {
+
+      describe('comments', function() {
+
+        it('should return same CSS for foo', function() {
+
+          return new Builder({
+              fs: gracefulFs
+          }).build({
+            lessInputPath: 'comments/themes/foo/library.source.less',
+            rootPaths: [
+              'test/fixtures/libraries/scopes/comments/lib1',
+              'test/fixtures/libraries/scopes/comments/lib2',
+              'test/fixtures/libraries/lib1'
+            ]
+          }).then(function(result) {
+            assert.equal(result.css, readFile('test/expected/libraries/scopes/comments/lib1/comments/themes/foo/library.css'), 'CSS scoping should be correctly generated');
+            assert.equal(result.cssRtl, readFile('test/expected/libraries/scopes/comments/lib1/comments/themes/foo/library-RTL.css'), 'Rtl CSS scoping should be correctly generated');
+          });
+
+        });
+
+    });
+
+});
