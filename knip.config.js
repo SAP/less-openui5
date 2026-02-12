f0ada1da0b91c08ac4d77233e08bbdc4f285da77
Gianluca Beil <g.beil@sap.com>
2026-02-12 14:17:58 +0100

    [INTERNAL] Add comments to knip.config.js

--- knip.config.js
+++ knip.config.js
@@ -1,6 +1,7 @@
 const config = {
 	/**
-	 * As we currently only need unused dependency checks, we disable all checks except for that
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
 	 */
 	rules: {
 		files: "off",
